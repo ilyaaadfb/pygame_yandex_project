b3e37ab2ea270eced64183208c24dac8ff594a91
ilyaaadfb <ilgri1908@bk.ru>
2023-12-19 22:39:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import sys
+from button import ImageButton
+
+pygame.init()
+
+WIDTH, HEIGHT = 600, 550
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Menu test")
+# main_background = pygame.image.load("background.jpeg")
+
+def main_menu():
+    pass
